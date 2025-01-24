1e448d88925c203e2839cb990da5b93ab1ab1bb6
saidali777 <148846780+saidali777@users.noreply.github.com>
2025-01-24 07:06:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Bot')
 API_ID = int(environ.get('API_ID', '12618934'))
 API_HASH = environ.get('API_HASH', '49aacd0bc2f8924add29fb02e20c8a16')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7548204664:AAGfgIjMoW6AGIDTYvqTgaLYkQWCSp3s8sQ")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7548204664:AAF9TMFkKQWm1JEXCFkjC1wk3vlo-5yuOv0")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
